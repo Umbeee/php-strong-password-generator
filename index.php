495d19f982418f4ed79ee61b6f369157156c8750
Umbeee <umberto.nesta@hotmail.com>
2023-05-23 17:22:04 +0200

    milestone 1 completa: logica completa

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
+$lunghezza = 0;
+$lunghezza = $_GET['variabileLunghezza'];
 
+//var_dump ($lunghezza);
+
+function passwordGen($lunghezza) {
+    $numLettere = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+ 
+    for ($i = 0; $i < $lunghezza; $i++) {
+        $indice = rand(0, strlen($numLettere) - 1);
+        $randomString .= $numLettere[$indice];
+    }
+ 
+    return $randomString;
+}
+ 
+echo passwordGen($lunghezza);
+?>
 
 
 ?>
@@ -10,9 +28,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>php-strong-password-generator</title>
 </head>
 <body>
     
+    <h1 class="text-center my-5 text-capitalize">strong password generator</h1>
+    <form action="index.php" class="text-center d-flex align-item-center justify-content-center gap-3">
+        <input type="number" placeholder="lunghezza password" name="variabileLunghezza">
+        <button class="btn btn-primary">invia</button>
+    </form>
+
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
